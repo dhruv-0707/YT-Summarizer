a683581b4a49cc9e661e1153e2e8408cf77ec8bf
Dhruv <dhruvnautiyal0707@gmail.com>
2025-01-15 23:13:35 +0530

    updated for deployement

--- api/nhost.js
+++ api/nhost.js
@@ -3,7 +3,6 @@ import axios from 'axios'; // For making HTTP requests
 import cors from 'cors';
 
 const app = express();
-const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
